dc527a4138bce0cb8d7fb1e94b23747f19e66932
Ebenezer Scrooge <1641367382@qq.com>
2020-12-08 08:34:59 +0800

    Add HyperGlock for test

--- cl_dll/ev_hldm.h
+++ cl_dll/ev_hldm.h
@@ -1,4 +1,4 @@
-//========= Copyright © 1996-2002, Valve LLC, All rights reserved. ============
+ï»¿//========= Copyright ?1996-2002, Valve LLC, All rights reserved. ============
 //
 // Purpose: 
 //
@@ -87,9 +87,9 @@ enum gauss_e {
 	GAUSS_DRAW
 };
 
-void EV_HLDM_GunshotDecalTrace( pmtrace_t *pTrace, char *decalName );
-void EV_HLDM_DecalGunshot( pmtrace_t *pTrace, int iBulletType );
-int EV_HLDM_CheckTracer( int idx, float *vecSrc, float *end, float *forward, float *right, int iBulletType, int iTracerFreq, int *tracerCount );
-void EV_HLDM_FireBullets( int idx, float *forward, float *right, float *up, int cShots, float *vecSrc, float *vecDirShooting, float flDistance, int iBulletType, int iTracerFreq, int *tracerCount, float flSpreadX, float flSpreadY );
+void EV_HLDM_GunshotDecalTrace(pmtrace_t* pTrace, char* decalName);
+void EV_HLDM_DecalGunshot(pmtrace_t* pTrace, int iBulletType);
+int EV_HLDM_CheckTracer(int idx, float* vecSrc, float* end, float* forward, float* right, int iBulletType, int iTracerFreq, int* tracerCount);
+void EV_HLDM_FireBullets(int idx, float* forward, float* right, float* up, int cShots, float* vecSrc, float* vecDirShooting, float flDistance, int iBulletType, int iTracerFreq, int* tracerCount, float flSpreadX, float flSpreadY);
 
 #endif // EV_HLDMH
